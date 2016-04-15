16f1ac466bc2748b69190062663c786d63297899
Cory House <housecor@gmail.com>
2016-04-15 15:52:08 -0500

    Initial Commit

---
+++ tools/startMessage.js
@@ -0,0 +1,6 @@
+import colors from 'colors';
+
+/*eslint-disable no-console */
+
+console.log('Starting app in dev mode...'.green);
+
---
+++ tools/testSetup.js
@@ -0,0 +1,43 @@
+// Tests are placed alongside files under test.
+// This file does the following:
+// 1. Registers babel for transpiling our code for testing
+// 2. Disables Webpack-specific features that Mocha doesn't understand.
+// 3. Requires jsdom so we can test via an in-memory DOM in Node
+// 4. Sets up global vars that mimic a browser.
+
+/*eslint-disable no-var*/
+
+// This assures the .babelrc dev config (which includes
+// hot module reloading code) doesn't apply for tests.
+process.env.NODE_ENV = 'production'; // this assures the .babelrc dev config doesn't apply.
+
+// Register babel so that it will transpile ES6 to ES5
+// before our tests run.
+require('babel-register')();
+
+// Disable webpack-specific features for tests since
+// Mocha doesn't know what to do with them.
+require.extensions['.css'] = function () {return null;};
+require.extensions['.png'] = function () {return null;};
+require.extensions['.jpg'] = function () {return null;};
+
+// Configure JSDOM and set global variables
+// to simulate a browser environment for tests.
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+}
+});
+
+global.navigator = {
+    userAgent: 'node.js'
+};
+
+documentRef = document;  //eslint-disable-line no-undef
