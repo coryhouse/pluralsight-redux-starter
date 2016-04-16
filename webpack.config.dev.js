abc787ac2a20a9bec886cca688dafd7e2a2f471b
Cory House <housecor@gmail.com>
2016-04-15 21:56:17 -0500

    Added client reload if hot module loading fails.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ export default {
   noInfo: false,
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client',
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     './src/index'
   ],
   target: 'web',
