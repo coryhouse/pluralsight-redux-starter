f71f3df28148e1250140669c0a18d40d3c50d286
Cory House <housecor@gmail.com>
2016-05-05 17:48:45 -0500

    Whitespace tweak

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -4,7 +4,7 @@ import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
 
-/*eslint-disable no-console */
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
@@ -17,11 +17,11 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
-app.listen(port, function (err) {
+app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
