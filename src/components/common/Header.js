926706eadfe374c04091bfcb5901e56733299105
Sijia Cao <sijiacao@Sijias-MacBook-Air.local>
2017-05-30 09:18:29 -0700

    Create Preloader component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
-
+import LoadingDots from './LoadingDots';
 //stateless function
 //to use the header, import it in app.js, and put <Header/> at where the header is wanted
 const Header = () => {
@@ -11,6 +11,7 @@ const Header = () => {
       <Link to="/courses" activeClassName="active">Courses</Link>
       {"|"}
       <Link to="/about" activeClassName="active">About</Link>
+      <LoadingDots interval={100} dots={20}/>
     </nav>
   );
 };
